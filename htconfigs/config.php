3a6cb33f1ba06680713ccba9ac26e14402c65959
Stlgy <110057231+Stlgy@users.noreply.github.com>
2022-08-12 13:12:44 +0100

    Add files via upload

---
+++ htconfigs/config.php
@@ -0,0 +1,6 @@
+<?php
+$host="127.0.0.1";
+$bd="terras";
+$user="root";
+$pass="";
+?>
---
+++ htconfigs/meu_conf.php
@@ -0,0 +1,8 @@
+<?php
+$server="localhost";
+$bd="avaliacao";
+$user="root";
+$pass="";
+$charset="utf8mb4";
+?>
+        
